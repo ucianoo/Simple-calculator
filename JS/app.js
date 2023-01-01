95fd38524cba40f843b8b506bc49b2783c4ce3cd
Uciano <ansonuchenna@gmail.com>
2023-01-01 22:24:15 +0000

    building a simple calculator;

---
+++ JS/app.js
@@ -0,0 +1,8 @@
+var n1 = document.getElementById("num1");
+var n2 = document.getElementById("num2");
+var res = document.getElementById("result")
+
+document.getElementById("add").addEventListener("click",
+function() {
+
+});
